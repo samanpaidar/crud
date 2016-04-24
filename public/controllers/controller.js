e2c693212f46701a4ca22059df48f5dee63a7f9f
samanpaidar <samanpaidar@gmail.com>
2016-04-24 19:30:19 +0200

    to start remove

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,9 +2,24 @@ var studensP = angular.module('studensP', []);
 studensP.controller('student', ['$scope', '$http', function($scope, $http) {
     
     console.log("hej hej");
-    $http.get('/studentlist').success(function (response) {
-    	console.log('got data');
-    	$scope.studentlist = response;
-    });
+    var refresh = function () {
+	    $http.get('/studentlist').success(function (response) {
+	    	console.log('got data');
+	    	$scope.studentlist = response;
+	    	$scope.student ="";
+	    });
+    };
+    refresh();
+
+    $scope.addStudent = function () {
+    	console.log($scope.student);
+    	$http.post('/studentlist',$scope.student).success(function (response) {
+    		console.log(response);
+    		refresh();
+    	});
+    };
+    $scope.remove = function (id) {
+    	console.log(id);
+    };
     
 }]);
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-var express = require('express');
-var app = express();
-var nodemon = require('nodemon');
-var mongojs= require('mongojs');
-var db = mongojs('studentlist',['studentlist']);
+var express     = require('express');
+var app         = express();
+var nodemon     = require('nodemon');
+var mongojs     = require('mongojs');
+var db          = mongojs('studentlist',['studentlist']);
+var bodyParser  = require('body-parser');
 
+app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public" ));
+
 app.get('/studentlist',function (req , res) {
 	console.log('i got req ');
 	db.studentlist.find(function (err,docs) {
 		console.log(docs);
 		res.json(docs);
+	});
+});
+app.post('/studentlist',function (req , res) {
+	console.log(req.body);
+	db.studentlist.insert(req.body,function (err , doc) {
+		res.json(doc);
 	});
 });
 
